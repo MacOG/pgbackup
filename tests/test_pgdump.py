fbf8381442203887e9120706843608b55f479194
MacOG <49504334+MacOG@users.noreply.github.com>
2020-01-02 15:33:48 +0000

    Create Test for pgdump

---
+++ tests/test_pgdump.py
@@ -0,0 +1,14 @@
+import pytest
+import subprocess
+
+from pgbackup import pgdump
+
+url = ""
+
+def test_dump_calls_pg_dump(mocker):
+    """
+    Utilize pg_dump with the database URL
+    """
+    mocker.patch('subprocess.Popen')
+    assert pgdump.dump(url)
+    subprocess.Popen.assert_called_with(['pg_dump', url], stdout=subprocess.PIPE)
